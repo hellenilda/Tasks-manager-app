ed9fd5161bf151d86ed95be6e785787efa61b6a3
hellenilda <hellen95limaaraujo@gmail.com>
2025-04-28 23:58:40 -0300

    :recycle: refactor: tamanho do h1 em AppTextStyles

--- lib/features/common/constants/app_text_styles.dart
+++ lib/features/common/constants/app_text_styles.dart
@@ -5,14 +5,14 @@ class AppTextStyles {
 
   // Texto da Splash Screen
   static const TextStyle textoGrande = TextStyle(
-    fontSize: 50.0, 
+    fontSize: 46.0, 
     fontFamily: 'Montserrat'
     // A propriedade de cor é adicionada com .copyWith(color: cor) na instância
   );
 
   static const TextStyle h1 = TextStyle(
     fontFamily: 'Montserrat',
-    fontSize: 28.0,
+    fontSize: 24.0,
     fontWeight: FontWeight.bold
   );
 
