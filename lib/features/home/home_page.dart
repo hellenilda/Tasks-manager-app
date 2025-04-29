c08fa048c26c39162ca3d750229a669e0820f240
hellenilda <hellen95limaaraujo@gmail.com>
2025-04-29 12:15:46 -0300

    :recycle: refactor: margens, cores e outros estilos do Home

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -8,87 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.cinzaBg,
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 20)
-            ),
-            Text(
-              'TAREFAS', 
-              style: AppTextStyles.h1,
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 20)
-            ),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: AppColors.cinza, // Cor da borda
-                  width: 1.0, // Espessura da borda
-                ),
-                borderRadius: BorderRadius.circular(19), // Mesmo valor do shape da SearchBar
-              ),
-              child: SearchBar(
-                hintText: 'Pesquise sua tarefa...',
-                hintStyle: WidgetStateProperty.all<TextStyle>(
-                  TextStyle(color: AppColors.cinza),
-                ),
-                textStyle: WidgetStateProperty.all<TextStyle>(
-                  TextStyle(
-                    fontFamily: 'Montserrat',
-                    fontSize: 16.0,
-                  ),
-                ),
-                backgroundColor: WidgetStateProperty.all<Color>(
-                  AppColors.branco,
-                ),
-                elevation: WidgetStateProperty.all<double>(0),
-                trailing: [
-                  IconButton(
-                    color: AppColors.lilas,
-                    icon: Icon(Icons.search, color: AppColors.cinza),
-                    onPressed: () {},
-                  ),
-                ],
-                padding: WidgetStateProperty.all<EdgeInsets>(
-                  EdgeInsets.symmetric(horizontal: 16),
-                ),
-                shape: WidgetStateProperty.all<OutlinedBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(19),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 20)
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.lilas, 
-                    foregroundColor: AppColors.branco,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(9.0)
-                    )
-                  ),
-                  onPressed: (){},
-                  child: Align(
-                    child: Text(
-                      'Adicionar', 
-                      style: AppTextStyles.p,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      backgroundColor: AppColors.azulBg,
       appBar: AppBar(
         title: Align(
           child: Text(
@@ -100,11 +20,114 @@ class HomePage extends StatelessWidget {
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(1.0), // Altura da borda
           child: Container(
-            color: AppColors.cinza, // Cor da borda
+            color: AppColors.azulBordas, // Cor da borda
             height: 1.0, // Espessura da borda
           ),
         ),
-        leading: Icon(Icons.person_outline_sharp),
+      ), 
+      drawer: Drawer(
+        backgroundColor: AppColors.branco,
+        child: Column(
+          children: [
+            Icon(Icons.person),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 20)
+            ),
+            Text(
+              'TAREFAS', 
+              style: AppTextStyles.h1,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: AppColors.azulBordas, // Cor da borda
+                    width: 1.0, // Espessura da borda
+                  ),
+                  borderRadius: BorderRadius.circular(19), // Mesmo valor do shape da SearchBar
+                ),
+                child: SearchBar(
+                  hintText: 'Pesquise sua tarefa...',
+                  hintStyle: WidgetStateProperty.all<TextStyle>(
+                    TextStyle(color: AppColors.azulBordas), // Cor da borda da SearchBar
+                  ),
+                  textStyle: WidgetStateProperty.all<TextStyle>(
+                    TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  backgroundColor: WidgetStateProperty.all<Color>(
+                    AppColors.branco, // Cor de fundo da SearchBar
+                  ),
+                  elevation: WidgetStateProperty.all<double>(0),
+                  trailing: [
+                    IconButton(
+                      color: AppColors.lilas,
+                      icon: Icon(Icons.search, color: AppColors.azulBordas), // Cor do ícone de pesquisa
+                      onPressed: () {},
+                    ),
+                  ],
+                  padding: WidgetStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 16),
+                  ),
+                  shape: WidgetStateProperty.all<OutlinedBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(19),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColors.branco,
+                  borderRadius: BorderRadius.circular(4)
+                ),
+                height: 60,
+                child: Center(
+                  child: Text(
+                    'Tarefa', 
+                    style: AppTextStyles.p,),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.lilas, 
+                      foregroundColor: AppColors.branco,
+                      minimumSize: Size(0, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(9.0)
+                      )
+                    ),
+                    onPressed: (){},
+                    child: Align(
+                      child: Text(
+                        'Adicionar', 
+                        style: AppTextStyles.p,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
